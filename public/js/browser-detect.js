bd86c7caec3ffa3a54c2215709a828c857f63206
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-06-03 11:42:27 +0100

    Update browser-detect.js
    release the browser-detect as Apache 2.0

--- public/js/browser-detect.js
+++ public/js/browser-detect.js
@@ -1,18 +1,20 @@
-/* ***************************************************************** */
-/*                                                                   */
-/* Licensed Materials - Property of IBM                              */
-/*                                                                   */
-/* (C) Copyright IBM Corp. 2001, 2014. All Rights Reserved.          */
-/*                                                                   */
-/* US Government Users Restricted Rights - Use, duplication or       */
-/* disclosure restricted by GSA ADP Schedule Contract with IBM Corp. */
-/*                                                                   */
-/* ***************************************************************** */
+/**
+ * Copyright 2014 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*global $:false */
 
-/*
-This code is made by James Zhang.
-This is a very basic browser sniffer.
-*/
 
 (function() {
 
