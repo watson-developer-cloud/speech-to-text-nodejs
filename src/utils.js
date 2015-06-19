a19f6b1627bd2ffef6b66053782b0af060a62622
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-06-19 18:14:36 -0400

    Reformat parseFile

--- src/utils.js
+++ src/utils.js
@@ -9,33 +9,33 @@ var fileBlock = function(_offset, length, _file, readChunk) {
   r.readAsArrayBuffer(blob);
 }
 
-// From alediaferia's SO response
+// Based on alediaferia's SO response
 // http://stackoverflow.com/questions/14438187/javascript-filereader-parsing-long-file-in-chunks
-exports.parseFile = function(file, ondata, onend, onerror) {
-    var fileSize   = file.size;
-    var chunkSize  = 2048; // bytes
-    var offset     = 0;
-    var self       = this; // we need a reference to the current object
-    var readChunk = function(evt) {
-        if (offset >= fileSize) {
-            console.log("Done reading file");
-            onend();
-            return;
-        }
-        if (evt.target.error == null) {
-            var buffer = evt.target.result;
-            var len = buffer.byteLength;
-            offset += len;
-            ondata(buffer); // callback for handling read chunk
-        } else {
-            var errorMessage = evt.target.error;
-            console.log("Read error: " + errorMessage);
-            onerror(errorMessage);
-            return;
-        }
-        fileBlock(offset, chunkSize, file, readChunk);
+exports.parseFile = function(options, ondata, onend, onerror) {
+  var file       = options.file;
+  var fileSize   = file.size;
+  var chunkSize  = options.bufferSize || 8192;
+  var offset     = 0;
+  var readChunk = function(evt) {
+    if (offset >= fileSize) {
+      console.log("Done reading file");
+      onend();
+      return;
+    }
+    if (evt.target.error == null) {
+      var buffer = evt.target.result;
+      var len = buffer.byteLength;
+      offset += len;
+      ondata(buffer); // callback for handling read chunk
+    } else {
+      var errorMessage = evt.target.error;
+      console.log("Read error: " + errorMessage);
+      onerror(errorMessage);
+      return;
     }
     fileBlock(offset, chunkSize, file, readChunk);
+  }
+  fileBlock(offset, chunkSize, file, readChunk);
 }
 
 
