99e631d5a682e16b2e73eed1af1ab89810631b74
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-06-20 15:44:53 -0400

    Add simple effects for images

--- src/views/effects.js
+++ src/views/effects.js
@@ -2,6 +2,7 @@
 
 
 exports.flashSVG = function(el) {
+  el.css({ fill: '#A53725' });
   function loop() {
     el.animate({ fill: '#A53725' },
         1000, 'linear')
@@ -9,11 +10,29 @@ exports.flashSVG = function(el) {
           1000, 'linear');
   }
   // return timer
-  return setTimeout(loop, 2000);
+  var timer = setTimeout(loop, 2000);
+  return timer;
 };
-
 
 exports.stopFlashSVG = function(timer) {
   el.css({ fill: 'white' } );
   clearInterval(timer);
 }
+
+exports.toggleImage = function(el, name) {
+  if(el.attr('src') == 'img/' + name + '.svg') {
+    el.attr("src", 'img/' + name + '-red.svg');
+  } else {
+    el.attr('src', 'img/' + name + '.svg');
+  }
+}
+
+var restoreImage = function(el, name) {
+  el.attr('src', 'img/' + name + '.svg');
+}
+
+exports.stopToggleImage = function(timer, el, name) {
+  clearInterval(timer);
+  restoreImage(el, name);
+}
+
