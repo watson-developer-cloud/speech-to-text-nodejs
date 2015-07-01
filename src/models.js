240e9ce799a338691cd1f7b91ab48f6dbfa024d3
ERIC S. BULLINGTON <esbullin@us.ibm.com>
2015-07-01 14:47:18 -0400

    Change stage URLs to production URLs

--- src/models.js
+++ src/models.js
@@ -1,7 +1,7 @@
 
 exports.getModels = function(options, callback) {
   var token = options.token;
-  var modelUrl = options.url || 'https://stream-s.watsonplatform.net/speech-to-text-beta/api/v1/models';
+  var modelUrl = options.url || 'https://stream.watsonplatform.net/speech-to-text/api/v1/models';
   var sttRequest = new XMLHttpRequest();
   sttRequest.open("GET", modelUrl, true);
   sttRequest.withCredentials = true;
