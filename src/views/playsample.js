d396dfbf2f5efe11623c92c698a24541a5e8651b
Ayman Kasaby <ayman.kasaby@gmail.com>
2015-10-12 15:21:03 -0400

    Update playsample.js

--- src/views/playsample.js
+++ src/views/playsample.js
@@ -25,34 +25,35 @@ var LOOKUP_TABLE = {
 var playSample = (function() {
 
   var running = false;
-  localStorage.setItem('currentlyDisplaying', false);
+  localStorage.setItem('currentlyDisplaying', 'false');
 
   return function(token, imageTag, iconName, url, callback) {
 
     $.publish('clearscreen');
+    var currentlyDisplaying = localStorage.getItem('currentlyDisplaying');
 
-    var currentlyDisplaying = JSON.parse(localStorage.getItem('currentlyDisplaying'));
-
-    console.log('CURRENTLY DISPLAYING', currentlyDisplaying);
+    console.log('CURRENTLY DISPLAYING:', currentlyDisplaying);
 
     // This error handling needs to be expanded to accomodate
     // the two different play samples files
-    if (currentlyDisplaying) {
+    if (currentlyDisplaying==='sample') {
       console.log('HARD SOCKET STOP');
       $.publish('socketstop');
-      localStorage.setItem('currentlyDisplaying', false);
+      localStorage.setItem('currentlyDisplaying', 'false');
       effects.stopToggleImage(timer, imageTag, iconName);
       effects.restoreImage(imageTag, iconName);
       running = false;
       return;
     }
-
-    if (currentlyDisplaying && running) {
-      showError('Currently another file is playing, please stop the file or wait until it finishes');
+    else if(currentlyDisplaying==='fileupload') {
+      showError('Currently, another file is playing, so please stop the file or wait until it finishes');
+        return;
+    }
+    else if(currentlyDisplaying==='record') {
+      showError('Currently, audio is being recorded, please stop recording first');
       return;
     }
-
-    localStorage.setItem('currentlyDisplaying', true);
+    localStorage.setItem('currentlyDisplaying', 'sample');
     running = true;
     
     $('#resultsText').val('');   // clear hypotheses from previous runs
@@ -83,7 +84,7 @@ var playSample = (function() {
           audio.pause();
           audio.currentTime = 0;
         });
-        handleFileUpload(token, currentModel, blob, contentType, function(socket) {
+        handleFileUpload('sample', token, currentModel, blob, contentType, function(socket) {
           var parseOptions = {
             file: blob
           };
@@ -115,7 +116,7 @@ var playSample = (function() {
           function(evt) {
             effects.stopToggleImage(timer, imageTag, iconName);
             effects.restoreImage(imageTag, iconName);
-            localStorage.getItem('currentlyDisplaying', false);
+            localStorage.getItem('currentlyDisplaying', 'false');
           }
         );
       };
