a1193564f0c6558275ec3b2e7667e8d72bea30fd
German Attanasio Ruiz <germanattanasio@gmail.com>
2015-10-22 10:41:19 -0400

    added a security file that will be only active if SECURE_EXPRESS is true

--- config/express.js
+++ config/express.js
@@ -18,66 +18,18 @@
 
 // Module dependencies
 var express    = require('express'),
-  favicon      = require('serve-favicon'),
-  errorhandler = require('errorhandler'),
-  secure       = require('express-secure-only'),
-  bodyParser   = require('body-parser'),
-  morgan       = require('morgan'),
-  csrf         = require('csurf'),
-  cookieParser = require('cookie-parser'),
-  fs           = require('fs');
+  bodyParser   = require('body-parser');
 
 module.exports = function (app) {
   app.set('view engine', 'ejs');
   app.enable('trust proxy');
 
-  var logStream = fs.createWriteStream(__dirname + '/../logs/access.log', {flags: 'a'});
-  // setup the logger
-  app.use('/api/', morgan('combined', {stream: logStream}));
-
-
-  // use only https
-  var env = process.env.NODE_ENV || 'development';
-  if ('production' === env) {
-    console.log('redirect http to https');
-    app.use(secure());
-    app.use(errorhandler());
-  }
+  // Only loaded when SECURE_EXPRESS is `true`
+  if (process.env.SECURE_EXPRESS)
+    require('./security')(app);
 
   // Configure Express
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-
-  // Setup static public directory
+  app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
+  app.use(bodyParser.json({ limit: '1mb' }));
   app.use(express.static(__dirname + '/../public'));
-  app.use(favicon(__dirname + '/../public/images/favicon.ico'));
-
-  // cookies
-  var secret = Math.random().toString(36).substring(7);
-  app.use(cookieParser(secret));
-
-  var errorMessage = {
-    error:'Too many requests, please try again in 30 seconds.',
-    code: 429
-  };
-
-  // rate limiting
-  var rateLimit = require('express-rate-limit');
-  var limiter = rateLimit({
-    windowMs: 30 * 1000, // seconds
-    delayMs: 0,
-    max: 3,
-    message: JSON.stringify(errorMessage),
-    global: false
-  });
-
-  // csrf
-  var csrfProtection = csrf({ cookie: true });
-  app.get('/', csrfProtection, function(req, res) {
-    res.render('index', { ct: req.csrfToken() });
-  });
-
-  // apply to all requests that begin with /api/
-  // csfr token and rate limiting.
-  app.use('/api/', csrfProtection, limiter);
 };
