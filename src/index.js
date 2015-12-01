10c2d929bfcd6fb5a52d9b3428b4c1c48c146ee7
Ayman Kasaby <ayman.kasaby@gmail.com>
2015-11-30 19:41:43 -0500

    playback support from query strings in URL

--- src/index.js
+++ src/index.js
@@ -57,6 +57,17 @@ $(document).ready(function() {
 
     // Save models to localstorage
     localStorage.setItem('models', JSON.stringify(models));
+    
+    //Check if playback functionality is invoked
+    localStorage.setItem('playbackON', false);
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for(var i=0; i< vars.length; i++) {
+      var pair = vars[i].split('=');
+      if(decodeURIComponent(pair[0]) === 'debug') {   
+        localStorage.setItem('playbackON',decodeURIComponent(pair[1]));
+      }
+    }
 
     // Set default current model
     localStorage.setItem('currentModel', 'en-US_BroadbandModel');
