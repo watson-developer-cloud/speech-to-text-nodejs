9f0ab7080c708644fcfa6df53d3f9b2707464461
Nathan Friedly <nathan@nfriedly.com>
2016-11-28 12:59:46 -0500

    fix bulleted list for keywords

--- views/keywords.jsx
+++ views/keywords.jsx
@@ -23,7 +23,7 @@ function renderSpottings(spottings) {
 
 export function Keywords(props) {
     const spotted = props.results.reduce(keywordReducer, {});
-    const list = props.keywords.map(k => (<li key={k}><b>{k}</b>: {renderSpottings(spotted[k])}</li>));
+    const list = props.keywords.map(k => (<li key={k} className="base--li"><b>{k}</b>: {renderSpottings(spotted[k])}</li>));
     // todo: Think about a better way of displaying this, perhaps in-line with the text and just highlight the appropriate word
     // todo: figure out why some styles such as base--ul_no-bullets base--p_light don't seem to work
     return (
@@ -31,7 +31,7 @@ export function Keywords(props) {
             <ul className="base--ul base--ul_no-bullets">
                 {list}
             </ul>
-            <p class="base--p base--p_light">Format for each spotted keyword is start time-end time (% confidence).</p>
+            <p className="base--p base--p_light">Format for each spotted keyword is start time-end time (% confidence).</p>
         </div>
     );
 }
