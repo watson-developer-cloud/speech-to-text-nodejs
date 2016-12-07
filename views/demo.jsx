da832bd7558201cc22c8940b5c310b6c9635f7aa
Nathan Friedly <nathan@nfriedly.com>
2016-12-07 14:54:09 -0500

    Update intro text to match current demo. Removes references to (unsupported) drag-and-drop.
    Fixes https://github.ibm.com/Watson/developer-experience/issues/1394

--- views/demo.jsx
+++ views/demo.jsx
@@ -210,15 +210,16 @@ export default React.createClass({
 
         return (<div className="_container _container_large">
             <h2>Transcribe Audio</h2>
-            <p className="base--p">Use your microphone (compatible only with <a className="base--a"
-                                                                                href="https://www.google.com/intl/en/chrome/browser/desktop/">Google
-                Chrome</a> and <a className="base--a" href="https://www.mozilla.org/en-US/firefox/new/">Mozilla
-                Firefox</a>).
-                Upload pre-recorded audio (WAV for uncompressed audio, FLAC or OPUS) file formats.
-                Drag and drop recorded audio onto the page, or use the audio samples provided.
-                The returned result includes the recognized text, word alternatives (aka confusion networks), and
-                spotted keywords.
-                You may choose to spot your keywords by entering them (separated by commas) in the text box.</p>
+
+            <ul className="base--ul">
+                <li className="base--li">Use your microphone to record audio (Chrome or Firefox only).</li>
+                <li className="base--li">Upload pre-recorded audio (.wav, .flac, or .opus only).</li>
+                <li className="base--li">Play one of the sample audio files.</li>
+            </ul>
+            <div style={{paddingRight: '3em', paddingBottom: '2em'}}>
+                The returned result includes the recognized text, word alternatives, and spotted keywords. Some models can detect multiple speakers; this may slow down performance.
+            </div>
+
 
             <h3>Setup</h3>
 
