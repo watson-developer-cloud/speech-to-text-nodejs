8932593df6a19d609026a0dbcbf9a6b450ee1657
Nathan Friedly <nathan@nfriedly.com>
2016-12-18 14:48:13 -0500

    enable keywords w/ speaker_labels

--- views/keywords.jsx
+++ views/keywords.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Icon } from 'watson-react-components';
 
+// reducer to convert a list of messages into a (flat) list of results
+function allResultsReducer(list, message) {
+    return list.concat(message.results);
+}
 
-function keywordReducer(keywords, msg) {
-    const result = msg.results[0];
+// reducer to extract all matched keywords from a list of results
+function keywordReducer(keywords, result) {
     Object.keys(result.keywords_result || {}).forEach(k => {
         keywords[k] = keywords[k] || [];
         keywords[k].push(...result.keywords_result[k]);
@@ -11,10 +15,14 @@ function keywordReducer(keywords, msg) {
     return keywords;
 }
 
+function getSpotted(messages) {
+    return messages.reduce(allResultsReducer, []).reduce(keywordReducer, {});
+}
+
 export function Keywords(props) {
     const notSpotted = props.isInProgress ? 'Not yet spotted.' : 'Not spotted.';
     const notSpottedIcon = props.isInProgress ? 'loader' : 'close';
-    const spotted = props.messages.reduce(keywordReducer, {});
+    const spotted = getSpotted(props.messages);
     const list = props.keywords.map(k => {
         const spottings = spotted[k];
         return (
@@ -37,8 +45,8 @@ export function Keywords(props) {
     );
 }
 
-export function getKeywordsSummary(keywords, results) {
-    const spotted = Object.keys(results.reduce(keywordReducer, {})).length;
+export function getKeywordsSummary(keywords, messages) {
+    const spotted = Object.keys(getSpotted(messages)).length;
     const total = keywords.length;
     return `(${spotted}/${total})`;
 }
