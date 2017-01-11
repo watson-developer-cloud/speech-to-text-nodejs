da1598338b01b3fe569dba0e01d2f54fbfb5cf87
Nathan Friedly <nfriedly@users.noreply.github.com>
2017-01-11 10:56:20 -0500

    Bluemix now uses PORT instead VCAP_APP_PORT

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ var app = require('./app.js');
 // Deployment tracking
 require('cf-deployment-tracker-client').track();
 
-var port = process.env.VCAP_APP_PORT || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('listening at:', port);
